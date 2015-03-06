2aa911688c35dc147527e8a28fa909667d2d5710
Anastasia Zorko <azorko13@gmail.com>
2015-03-06 08:27:31 -0800

    add readme

--- rating.js
+++ rating.js
@@ -1,48 +1,70 @@
-var Rating = function (el, doc) {
-	this.el = el;
+var Rating = function (elRating, elDisplay, doc) {
+	this.elRating = elRating;
+	this.elDisplay = elDisplay;
 	this.doc = doc;
-	this.instantiate();
+	this.instantiateContainer(this.elRating);
+	this.instantiateContainer(this.elDisplay);
 	this.currScore = 0;
 	this.sumScore = 0;
 	this.avgScore = 0;
 	this.votes = 0;
 	
-	this.el.addEventListener("click", this.changeState.bind(this));
+	this.elRating.addEventListener("click", this.updateRating.bind(this));
 };
 
 Rating.prototype.addScore = function () {
 	this.votes++;
 	this.sumScore += this.currScore;
 	this.avgScore = this.sumScore / this.votes;
+	this.updateDisplay();
 };
 
-Rating.prototype.changeState = function (event) {
-	var star = event.target, currentStar = this.el.firstChild, i;
-	
-	while (currentStar !== null) {
-		currentStar.classList.remove("full-star");
-		currentStar = currentStar.nextElementSibling;
+Rating.prototype.clearStars = function (container) {
+	var currStar = container.firstChild;
+	while (currStar !== null) {
+		currStar.classList.remove("full-star", "half-star");
+		currStar = currStar.nextElementSibling;
 	}
+};
+
+Rating.prototype.getScore = function () {
+	if (this.avgScore > Math.floor(this.avgScore)) { return Math.floor(this.avgScore) + 0.5; }
+	return this.avgScore;
+};
+
+Rating.prototype.instantiateContainer = function (container) {
+	var i, star;
+	for (i = 0; i < 5; i++) {
+		star = this.doc.createElement("span");
+		star.classList.add("empty-star", "star");
+		container.appendChild(star);
+	}
+	container.classList.add("rating-container");
+};
+
+Rating.prototype.updateDisplay = function () {
+	var i, currStar;
 	
-	currentStar = this.el.firstChild;
+	this.clearStars(this.elDisplay);
+	
+	currStar = this.elDisplay.firstChild;
+	for (i = 0; i < Math.floor(this.avgScore); i++) {
+		currStar.classList.add("full-star");
+		currStar = currStar.nextElementSibling;
+	}
+	if (this.avgScore > Math.floor(this.avgScore)) { currStar.classList.add("half-star"); }
+};
+
+Rating.prototype.updateRating = function (event) {
+	var star = event.target, currentStar = this.elRating.firstChild, i;
+	
+	this.clearStars(this.elRating);
+	
+	currentStar = this.elRating.firstChild;
 	this.currScore = 0;
 	while (currentStar !== null && star !== currentStar.previousElementSibling) {
 		currentStar.classList.add("full-star");
 		currentStar = currentStar.nextElementSibling;
 		this.currScore++;
 	}
-};
-
-Rating.prototype.instantiate = function () {
-	var i, star;
-	for (i = 0; i < 5; i++) {
-		star = this.doc.createElement("span");
-		star.classList.add("empty-star", "star");
-		this.el.appendChild(star);
-	}
-	this.el.classList.add("rating-container");
-};
-
-Rating.prototype.score = function () {
-	return
 };
